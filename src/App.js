6e32f90cd8162a211ce72e4cd8aa7e9da8306261
ashish <ashishaggarwal406@yahoo.in>
2020-10-12 18:12:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import stock from './data'
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      stock :[],
+      selectRecord:[],
+      clickPriceId:null
+    }
+  }
+  componentWillMount(){
+    this.setState({stock});  
+  }
+  selectRecord = (stockId)=>{
+    let selectedRecord = this.state.selectRecord;
+    console.log(selectedRecord,stockId);
+    let index = selectedRecord.findIndex((s)=>s === stockId);
+    if(index > -1){
+      selectedRecord.splice(index,1);
+    }else{
+      selectedRecord.push(stockId);
+    }
+    this.setState(selectedRecord);
+  }
+  deleteRecord = ()=>{
+    let finalRecord = this.state.stock.filter(s=>{
+      return this.state.selectRecord.indexOf(s.id) === -1
+    })
+    this.setState({stock:finalRecord})
+  }
+  resetSelectedRecord = ()=>{
+    this.setState({selectRecord:[]})
+  }
+  nameSearch = (e)=>{
+    let searchValue = e.target.value;
+    if(e.target.value !== ""){
+      let finalRecord = this.state.stock.filter(s=>{
+        return s.name.includes(searchValue) 
+      })
+      this.setState({stock:finalRecord})
+    }else{
+      this.setState({stock})
+    }
+  }
+  render(){
+    // console.log("")
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+            <td></td>
+              <td>ID</td>
+              <td>
+                Name
+                <div>
+                  <input type="text" name="name" onChange={(e)=>{this.nameSearch(e)}}/>
+                </div>
+              </td>
+              <td>Price</td>
+              <td>Coupon</td>
+              <td>In Stock</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.stock.map((s)=>{
+                return (
+                  <tr key={s.id}>
+                    <td>
+                      <input 
+                        type="checkbox" 
+                        onChange={()=>this.selectRecord(s.id)}
+                        checked={this.state.selectRecord.indexOf(s.id) > -1?true:false}
+                      />
+                    </td>
+                    <td>{s.id}</td>
+                    <td>{s.name}</td>
+                    <td 
+                      contentEditable={true}
+                    >
+                      {s.price}
+                    </td>
+                    <td>{s.coupon}</td>
+                    <td>{s.inStock}</td>
+                  </tr>  
+                )
+              })
+            }
+          </tbody>
+        </table>
+        <button onClick={this.deleteRecord}>Delete</button>
+        <button onClick={this.resetSelectedRecord}>Reset</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,46 @@
+let stock = [
+    {
+        id:0,
+        name:"Item name 1",
+        price:2100,
+        coupon:"yes",
+        inStock:"yes"
+    },
+    {
+        id:1,
+        name:"Item name 2",
+        price:2100,
+        coupon:"yes",
+        inStock:"yes"
+    },
+    {
+        id:2,
+        name:"Item name 3",
+        price:2100,
+        coupon:"yes",
+        inStock:"yes"
+    },
+    {
+        id:3,
+        name:"Item name 4",
+        price:2100,
+        coupon:"yes",
+        inStock:"yes"
+    },
+    {
+        id:4,
+        name:"Item name 5",
+        price:2100,
+        coupon:"yes",
+        inStock:"yes"
+    },
+    {
+        id:5,
+        name:"Item name 6",
+        price:2100,
+        coupon:"yes",
+        inStock:"yes"
+    }
+]
+
+export default stock;
